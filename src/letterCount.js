298f9daca75c3a2c09dd143ee60bc6eb727c4a35
unknown <d_bille@yahoo.com>
2020-09-03 08:58:04 -0500

    adding multiple test cases to function

--- src/letterCount.js
+++ src/letterCount.js
@@ -1,6 +1,13 @@
 // 'cat' -> { c: 1, a: 1, t: 1 }
 // 'better' -> { b: 1, e: 2, t: 2, r: 1 }
 
-export const getLetterCount = (string)=> {
-    return {};
-}
+export const getLetterCount = (string) => {
+  return string.split("").reduce((obj, letter) => {
+    if (!obj[letter]) {
+      obj[letter] = 1;
+    } else {
+      obj[letter]++;
+    }
+    return obj;
+  }, {});
+};
--- src/letterCount.test.js
+++ src/letterCount.test.js
@@ -7,4 +7,14 @@ describe('getLetterCount - basic functionality', ()=> {
         const actual = getLetterCount('');
         expect(actual).to.deep.equal(expected);
     })
+    it('return correct letter count for a word with only one of each letter', ()=>{
+        const expected = { c: 1, a: 1, t: 1 }
+        const actual = getLetterCount('cat')
+        expect(actual).to.deep.equal(expected)
+    })
+    it('return correct letter count for a word with multiple instances of one letter', ()=>{
+        const expected = { m: 1, i: 4, s: 4, p: 2 };
+        const actual = getLetterCount('mississippi')
+        expect(actual).to.deep.equal(expected)
+    })
 })
