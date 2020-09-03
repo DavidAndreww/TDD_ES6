d0b38e3808b63e65455d142d79c3ce7c97816883
unknown <d_bille@yahoo.com>
2020-09-03 09:32:11 -0500

    prettier

--- src/anagrams.js
+++ src/anagrams.js
@@ -6,7 +6,7 @@ export const isAnagram = (string1, string2) => {
   if (!string1 || !string2) return false;
   if (typeof string1 !== "string" || typeof string2 !== "string") return false;
 
-  let letters1 = string1.replace(/\W+/g, '').split("").sort().join("");
+  let letters1 = string1.replace(/\W+/g, '').toLowerCase().split("").sort().join("");
   let letters2 = string2.replace(/\W+/g, '').split("").sort().join("");
 
   return letters1 === letters2 ? true : false;
